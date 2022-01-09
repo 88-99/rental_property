b6e3f02cce35da64ceb65acecec686cffab03a66
KW <kw@KWnoMacBook-Pro.local>
2022-01-10 00:02:13 +0900

    PropertyModelを修正

--- app/models/property.rb
+++ app/models/property.rb
@@ -1,6 +1,6 @@
 class Property < ApplicationRecord
-  has_many :stations, dependent: :destroy, inverse_of: :property
-  accepts_nested_attributes_for :stations, allow_destroy: true, reject_if: :all_blank
+  has_many :stations, dependent: :destroy
+  accepts_nested_attributes_for :stations, reject_if: :all_blank
   validates :name, presence: true
   validates :rent, presence: true
   validates :address, presence: true
